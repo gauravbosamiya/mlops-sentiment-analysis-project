944c3dffdaeed7d6650418352d2c8a38b2080755
Gaurav <gauravbosamiya9@gmail.com>
2025-04-01 10:19:38 +0530

    CI/CD - 13

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -2,25 +2,23 @@ import unittest
 from flask_app.app import app
 
 class FlaskAppTests(unittest.TestCase):
-    
+
     @classmethod
     def setUpClass(cls):
         cls.client = app.test_client()
-        
+
     def test_home_page(self):
-        res = self.client.get('/')
-        self.assertEqual(res.status_code, 200)
-        self.assertIn(b'<title>Sentiment Analysis</title>', res.data)
-        
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<title>Sentiment Analysis</title>', response.data)
+
     def test_predict_page(self):
-        res = self.client.post('/prredict', data=dict(text="I love this!"))
-        self.assertEqual(res.status_code, 200)
+        response = self.client.post('/predict', data=dict(text="I love this!"))
+        self.assertEqual(response.status_code, 200)
         self.assertTrue(
-            b'Positive' in res.data or b'Negative' in res.data,
+            b'Positive' in response.data or b'Negative' in response.data,
             "Response should contain either 'Positive' or 'Negative'"
-            )
-        
-if __name__=="__main__":
+        )
+
+if __name__ == '__main__':
     unittest.main()
-        
-    
